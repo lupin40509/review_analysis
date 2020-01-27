33c3151643a5b919daf8adfe8aa0b8758cc9ff1b
lupin Chen <lupin10502@gmail.com>
2020-01-28 00:55:01 +0800

    add Average function

--- read.py
+++ read.py
@@ -1,8 +1,13 @@
-data = []
-count = 0
+data = [] #存入 data
+count = 0 #留言計次
+w_count = 0 #字數計次
+
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
+		w_count += len(line)
 		count += 1 
-		if count % 1000 == 0:
-			print(len(data))	
+
+print('檔案讀取完成,總共有', len(data),'筆資料')	
+#print('共有%d字元' % w_count)
+print('每筆留言平均長度為 %.2f' % (w_count / count))
